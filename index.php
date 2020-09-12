ce6367b70834142087d21945ca1f6d5284598288
llahti <llahti@student.hive.fi>
2020-09-12 18:00:32 +0300

    Init project

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="index.css" />
+</head>
+<body>
+	<div class="main-container">
+		<img id="img" src="imgs/loading.png" />
+		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo necessitatibus sequi rerum, architecto vero neque modi! Velit repellat repellendus necessitatibus beatae, maxime accusamus dicta recusandae tempore voluptatum, eum labore vero!</p>
+	</div>
+</body>
+</html>

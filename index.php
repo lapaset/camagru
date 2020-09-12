856d10ae2c57d2d5c28ab0d875d2f8d3f0ac2204
llahti <llahti@student.hive.fi>
2020-09-12 20:09:11 +0300

    Frontpage layout

--- index.php
+++ index.php
@@ -6,9 +6,21 @@
 	<link rel="stylesheet" type="text/css" href="index.css" />
 </head>
 <body>
+	<div class="header">
+		<h1>camagru</h1>
+	</div>
 	<div class="main-container">
-		<img id="img" src="imgs/loading.png" />
-		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo necessitatibus sequi rerum, architecto vero neque modi! Velit repellat repellendus necessitatibus beatae, maxime accusamus dicta recusandae tempore voluptatum, eum labore vero!</p>
+		<div class="img-container">
+			<img id="img" src="imgs/loading.png" />
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo necessitatibus sequi rerum, architecto vero neque modi! Velit repellat repellendus necessitatibus beatae, maxime accusamus dicta recusandae tempore voluptatum, eum labore vero!</p>
+		</div>
+		<div class="img-container">
+			<img id="img" src="imgs/loading.png" />
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo necessitatibus sequi rerum, architecto vero neque modi! Velit repellat repellendus necessitatibus beatae, maxime accusamus dicta recusandae tempore voluptatum, eum labore vero!</p>
+		</div>
+	</div>
+	<div class="footer">
+		<p>add pic</p>
 	</div>
 </body>
 </html>

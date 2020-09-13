55fb14ef38328cbd0d8cc22c61938b30f331a25a
llahti <llahti@student.hive.fi>
2020-09-13 21:47:02 +0300

    Edit layout

--- index.php
+++ index.php
@@ -8,15 +8,11 @@
 </head>
 <body>
 	<div class="header">
-		<div class="logo-container">
-			<img id="logo" src="imgs/logo8.png" title="camagru logo" alt="camagru logo" />
-		</div>
-		<div class="login-controls">
-			<a href="#" alt="login">login</a> | 
-			<a href="#" alt="register">sign up</a>
-		</div>
+		<a id="register" href="#" alt="register" title="register">register</a>
 	</div>
+
 	<div class="main-container">
+		<h1>camagru</h1>
 		<div class="img-container">
 			<img id="img" src="imgs/loading.png" />
 			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo necessitatibus sequi rerum, architecto vero neque modi! Velit repellat repellendus necessitatibus beatae, maxime accusamus dicta recusandae tempore voluptatum, eum labore vero!</p>
@@ -26,16 +22,40 @@
 			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo necessitatibus sequi rerum, architecto vero neque modi! Velit repellat repellendus necessitatibus beatae, maxime accusamus dicta recusandae tempore voluptatum, eum labore vero!</p>
 		</div>
 	</div>
+
+	<a href="srcs/editor.php" alt="add photo" title="add photo">
+		<div class="circle-container">
+			<img id="add" src="imgs/add.png" title="add photo" alt="add photo" />
+		</div>
+	</a>
+
 	<div class="footer">
-		<div class="footer-side"></div>
-		<div class="footer-mid">
-			<a href="srcs/editor.php" alt="add photo" title="add photo">
-				<img class="footer-icon" src="imgs/add_a_photo.png" title="add photo" alt="add photo" />
+		<div class="footer-img">
+			<a href="#" alt="settings" title="settings">
+				<img id="settings-icon" src="imgs/settings.png" title="settings" alt="settings" />
 			</a>
 		</div>
-		<div class="footer-side">
+		<div class="footer-img">
+			<a href="#" alt="login" title="login">
+				<img id="login-icon" src="imgs/login.png" title="login" alt="login" />
+			</a>
+		</div>
+	</div>
+
+	<div class="mobile-footer">
+		<div class="mobile-footer-img">
 			<a href="#" alt="settings" title="settings">
-				<img class="footer-icon" src="imgs/settings.png" title="settings" alt="settings" />
+				<img id="settings-icon" src="imgs/settings.png" title="settings" alt="settings" />
+			</a>
+		</div>
+		<div class="mobile-footer-img">
+			<a href="srcs/editor.php" alt="add photo" title="add photo">
+				<img id="add-icon" src="imgs/add.png" title="add photo" alt="add photo" />
+			</a>
+		</div>
+		<div class="mobile-footer-img">
+			<a href="#" alt="login" title="login">
+				<img id="login-icon" src="imgs/login.png" title="login" alt="login" />
 			</a>
 		</div>
 	</div>
--- srcs/editor.php
+++ srcs/editor.php
@@ -8,14 +8,10 @@
 </head>
 <body>
 	<div class="header">
-		<div class="logo-container">
-			<a href="../index.php" title="frontpage" >
-				<img id="logo" src="../imgs/logo8.png" title="camagru logo" alt="camagru logo" />
+		<div class="frontpage-link">
+			<a href="../index.php" alt="frontpage" title="frontpage">
+				<img id="back-icon" src="../imgs/back.png" title="back" alt="back" />
 			</a>
-		</div>
-		<div class="login-controls">
-			<a href="#" alt="login">login</a> | 
-			<a href="#" alt="register">sign up</a>
 		</div>
 	</div>
 	<div class="main-container">
@@ -40,18 +36,36 @@
 
 		<canvas id="canvas"></canvas>
 	</div>
+
 	<div class="footer">
-		<div class="footer-side">
-			<a href="../index.php" alt="frontpage" title="frontpage">
-				<img class="footer-icon" src="../imgs/back.png" title="frontpage" alt="frontpage" />
+		<div class="footer-img">
+			<a href="#" alt="settings" title="settings">
+				<img id="settings-icon" src="../imgs/settings.png" title="settings" alt="settings" />
 			</a>
 		</div>
-		<div class="footer-mid"></div>
-		<div class="footer-side">
-			<a href="#" alt="settings" title="settings">
-				<img class="footer-icon" src="../imgs/settings.png" title="settings" alt="settings" />
+		<div class="footer-img">
+			<a href="#" alt="logout" title="logout">
+				<img id="logout-icon" src="../imgs/logout.png" title="logout" alt="logout" />
 			</a>
 		</div>
 	</div>
+
+	<div class="mobile-footer">
+		<div class="mobile-footer-img">
+			<a href="../index.php" alt="frontpage" title="frontpage">
+				<img id="back-icon" src="../imgs/back.png" title="back" alt="back" />
+			</a>
+		</div>
+		<div class="mobile-footer-img">
+			<input type="image" id="take-photo-icon" src="../imgs/lens.png" title="take photo" alt="take photo" disabled/>
+			<input type="image" id="refresh-icon" src="../imgs/refresh.png" title="new photo" alt="new photo" />
+		</div>
+		<div class="mobile-footer-img">
+			<a href="#" alt="logout" title="logout">
+				<img id="logout-icon" src="../imgs/logout.png" title="logout" alt="logout" />
+			</a>
+		</div>
+	</div>
+
 </body>
 </html>

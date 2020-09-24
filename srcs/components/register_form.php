161e4ab1fc5919e80b814faff02abc66f17615b0
llahti <llahti@student.hive.fi>
2020-09-24 19:15:36 +0300

    Fix all the fields to be mandatory

--- srcs/components/register_form.php
+++ srcs/components/register_form.php
@@ -1,19 +1,19 @@
 <?php
-    $register_form = '
-        <h1>register</h1>
-        <div class="form-container">
-            <form method="post">
-                <label>Username</label><br />
-                <input id="username" type="text" name="login"
-                        value="'.$_POST['login'].'" /><br />
-                <label>Email</label><br />
-                <input type="email" name="email"
-                        value="'.$_POST['email'].'" /><br />
-                <label>Password</label><br />
-                <input type="password" id="pw" name="pw" value="" />
-				<input type="password" id="confirm_pw" name="confirm_pw" value="" placeholder="repeat password" />
+    $register_form = '<div class="form-container">
+                        <form method="post">
+                            <label>Username</label><br />
+                            <input id="username" type="text" name="login"
+                                    value="'.$_POST['login'].'" required /><br />
+                            <label>Email</label><br />
+                            <input type="email" name="email"
+                                    value="'.$_POST['email'].'" required /><br />
+                            <label>Password</label><br />
+                            <input type="password" id="pw" name="pw" value="" required />
+                            <input type="password" id="confirm_pw" name="confirm_pw" value="" placeholder="repeat password" required />
 
-                <input type="submit" value="OK" />
-            </form>
-        </div>';
+                            <input type="submit" value="OK" />
+                        </form>
+                    </div>
+                    <script src="js/validateUsername.js"></script>
+                    <script src="js/validatePassword.js"></script>';
 ?>

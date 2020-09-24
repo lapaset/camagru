fd3f20149221a7071463860a30c84f8516614446
llahti <llahti@student.hive.fi>
2020-09-24 19:17:52 +0300

    Fix layout and field requirement

--- srcs/components/forgot_password_form.php
+++ srcs/components/forgot_password_form.php
@@ -1,10 +1,8 @@
 <?php
-	$forgot_password_form = '<h1>forgot</h1>
-							<h2>send new password</h2>
-							<div class="form-container">
+	$forgot_password_form = '<div class="form-container">
 								<form method="post">
 									<label>Email</label><br />
-									<input type="email" name="email" /><br />
+									<input type="email" name="email" required/><br />
 									<input type= "submit" value="OK" />
 								</form>
 							</div>';

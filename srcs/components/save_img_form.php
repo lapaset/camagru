00bbe99ee721fcd916b375503ecaea261f13482d
llahti <llahti@student.hive.fi>
2020-12-15 22:43:20 +0200

    Change description field layout

--- srcs/components/save_img_form.php
+++ srcs/components/save_img_form.php
@@ -1,9 +1,9 @@
 <div class="form-container">
-	<form id="save-form" method="post" action="save_img.php">
+	<form id="save-form" method="post" action="save_img.php" autocomplete="off">
 		<input type="hidden" id="filters" name="filters" />
 		<input type="hidden" id="image-data" name="image-data" />
-		<input type="text" id="description" name="description"
+		<input id="description-field" type="text" id="description" name="description"
 			value="" placeholder="description" maxlength="160" />
-		<input type="button" onclick="save()" value="save" />
+		<input id="save-button" type="button" onclick="save()" value="save" />
 	</form>
 </div>

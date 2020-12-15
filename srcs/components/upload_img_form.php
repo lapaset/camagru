b4e01e0318e952ce672f06fc0f6eba856be0fc55
llahti <llahti@student.hive.fi>
2020-12-15 22:42:18 +0200

    Update form text

--- srcs/components/upload_img_form.php
+++ srcs/components/upload_img_form.php
@@ -1,6 +1,6 @@
 <div id="upload-form">
 	<div class="form-container">
-		select at least one filter or upload an image to take a photo<br />
+		Take a photo or upload one
 		(recommended size 600 x 400 px)<br />
 		<input type="file" id="image-loader" name="imageLoader"
 			accept="image" />
